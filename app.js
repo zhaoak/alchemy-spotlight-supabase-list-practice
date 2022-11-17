0aa30ae353e7023654ac83a669dc4b7b1cec0937
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-17 11:50:44 -0800

    forgot to commit sorry also wrote generic display and fetch function, render mock user data function

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 /* Imports */
+import { fetchData } from './fetch-utils.js';
+import {
+    renderUserData,
+    renderGroceryData,
+    renderCatBreeds,
+    renderCompanies,
+} from './render-utils.js';
 
 /* Get DOM Elements */
+const userDataEl = document.getElementById('user-data-list');
+const groceryDataEl = document.getElementById('grocery-data-list');
 
 /* State */
+let mock_user_data = [];
+let mock_grocery_data = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    mock_user_data = await fetchData('mock_users');
+    mock_grocery_data = await fetchData('grocery_items');
+
+    displayData(mock_user_data, userDataEl, renderUserData);
+    displayData(mock_grocery_data, groceryDataEl, renderGroceryData);
+});
 
 /* Display Functions */
+function displayData(source, appendLocation, renderFunc) {
+    appendLocation.textContent = '';
+    for (let row of source) {
+        const rowEl = renderFunc(row);
+        appendLocation.append(rowEl);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
---
+++ render-utils.js
@@ -0,0 +1,22 @@
+export function renderUserData(user) {
+    const userDataEl = document.createElement('div');
+    const fullName = document.createElement('h3');
+    const email = document.createElement('p');
+    const username = document.createElement('p');
+    const ip = document.createElement('p');
+
+    fullName.textContent = `${user.first_name} ${user.last_name}`;
+    email.textContent = user.email;
+    username.textContent = user.username;
+    ip.textContent = user.ip_address;
+
+    userDataEl.append(fullName, email, username, ip);
+    //userDataEl.classList.add('user-data-box');
+    return userDataEl;
+}
+
+export function renderGroceryData(item) {}
+
+export function renderCatBreeds(breed) {}
+
+export function renderCompanies(company) {}
